7769cea65400ed7b44a3ea97de58b1520aa6ea16
Lucas Ribeiro <lucas.and.rib@gmail.com>
2023-11-20 13:37:04 -0300

    Firebase auth and host added.

--- smart-apiary.cpp
+++ smart-apiary.cpp
@@ -6,3 +6,25 @@
 #include <WiFi.h>
 #include <time.h>
 #include <DHT.h>
+
+// Cloud database connection - Firebase, host and auth:
+#define FIREBASE_AUTENTICADOR "AIzaSyAW9ZLQhBTjqRVtM-m-oyHLqBTTNqZAOMk"
+#define FIREBASE_HOST "https://smart-apiary-default-rtdb.firebaseio.com"
+
+FirebaseData firebaseData;
+FirebaseJson json_measured_variables;
+
+void setup() {
+    Serial.begin(9600);
+
+    // Cloud database connection - Firebase:
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTENTICADOR);
+    Firebase.reconnectWiFi(true);
+
+    // Delay to capture the first measure:
+    delay(2500);
+}
+
+void loop() {
+    
+}
