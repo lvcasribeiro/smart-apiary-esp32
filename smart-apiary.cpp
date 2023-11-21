bafb81cf2b13c39437da8536ce3e23e24f8a53e2
Lucas Ribeiro <lucas.and.rib@gmail.com>
2023-11-21 18:16:28 -0300

    Removes sensitive information, updates incorrect #define's and adds debug variable.

--- smart-apiary.cpp
+++ smart-apiary.cpp
@@ -8,8 +8,8 @@
 #include <DHT.h>
 
 // Cloud database connection - Firebase, host and auth:
-#define FIREBASE_AUTENTICADOR "AIzaSyAW9ZLQhBTjqRVtM-m-oyHLqBTTNqZAOMk"
-#define FIREBASE_HOST "https://smart-apiary-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTENTICADOR "firebase_auth"
+#define FIREBASE_HOST "firebase_url"
 
 FirebaseData firebaseData;
 FirebaseJson json_measured_variables;
@@ -21,19 +21,20 @@ const int daylightOffset_sec = 0;
 const long gmtOffset_sec = -3600*3;
 
 // Wi-fi connection, network SSID and password:
-#define WIFI_SSID "apiario"
-#define WIFI_PASSWORD "12345678"
+#define WIFI_SSID "your_wi_fi_ssid"
+#define WIFI_PASSWORD "your_wi_fi_password"
 
 // Global variables:
 unsigned long time_now = 0;
+const int debug = 1;
 
 // Digital pinout:
 #define DHT_PIN 4
 #define SENT_PIN 15
 #define FAIL_PIN 2
 #define HEART_BEAT_PIN 19
-#define LOADCELL_DOUT_PIN = 18;
-#define LOADCELL_SCK_PIN = 5;
+#define LOADCELL_DOUT_PIN 18
+#define LOADCELL_SCK_PIN 5
 
 // Analog pinout:
 #define KY038_PIN 34
@@ -61,18 +62,24 @@ void setup() {
 
     // Wi-fi connection:
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("[!] Connecting to the wi-fi network..");
+    if(debug >= 1) {
+        Serial.print("[!] Connecting to the wi-fi network..");
+    }
 
     while(WiFi.status() != WL_CONNECTED) {
-        Serial.print(".");
+        if(debug >= 1) {
+            Serial.print(".");
+        }
         delay(2500);
     }
 
     // Identification of connected local IP:
-    Serial.println();
-    Serial.print("[!] Connected to wi-fi network, IP address: ");
-    Serial.println(WiFi.localIP());
-
+    if(debug >= 1) {
+        Serial.println();
+        Serial.print("[!] Connected to wi-fi network, IP address: ");
+        Serial.println(WiFi.localIP());
+    }
+    
     // Initialization of the timestamp capture - ntp server:
     configTime(gmtOffset_sec, daylightOffset_sec, NTP_SERVER);
 
@@ -105,7 +112,9 @@ void loop() {
 
     // Check if any reads failed and exit early to try again:
     if (isnan(humidity) || isnan(temperature)) {
-        Serial.println("[!] Failed to read from DHT sensor!");
+        if(debug >= 1) {
+            Serial.println("[!] Failed to read from DHT sensor!");
+        }
         return;
     }
 
@@ -129,7 +138,9 @@ void loop() {
     struct tm time_info;
 
     if (!getLocalTime(&time_info)) {
-        Serial.println("[!] Failed to capture NTP server data.");
+        if(debug >= 1) {
+            Serial.println("[!] Failed to capture NTP server data.");
+        }
         return;
     }
 
@@ -138,19 +149,21 @@ void loop() {
     strftime(current_time, 15, "%H:%M:%S", &time_info);
 
     // Debug:
-    Serial.print("Temperature: ");
-    Serial.print(temperature_rounded);
-    Serial.print(" *C | Humidity: ");
-    Serial.print(humidity_rounded);
-    Serial.print("% | Heat index: ");
-    Serial.print(heat_index_rounded);
-    Serial.print(" *C | Sound intensity: ");
-    Serial.print(sound_decibels);
-    Serial.print(" dB | Luminosity: ");
-    Serial.print(luminosity_percentage_rounded);
-    Serial.print("% | Load: ");
-    Serial.print(load);
-    Serial.println(" kg.");
+    if(debug >= 2) {
+        Serial.print("Temperature: ");
+        Serial.print(temperature_rounded);
+        Serial.print(" *C | Humidity: ");
+        Serial.print(humidity_rounded);
+        Serial.print("% | Heat index: ");
+        Serial.print(heat_index_rounded);
+        Serial.print(" *C | Sound intensity: ");
+        Serial.print(sound_decibels);
+        Serial.print(" dB | Luminosity: ");
+        Serial.print(luminosity_percentage_rounded);
+        Serial.print("% | Load: ");
+        Serial.print(load);
+        Serial.println(" kg.");
+    }
 
     // Values update, DHT --> cloud database:
     if(Firebase.get(firebaseData, "/measured_variables")) {
@@ -174,9 +187,11 @@ void loop() {
             }
         }
     } else {
-        Serial.print("[!] Error updating cloud data: [FIREBASE LATENCY][ttl] - ");
-        Serial.print(firebaseData.errorReason());
-        Serial.println(".");
+        if(debug >= 1) {
+            Serial.print("[!] Error updating cloud data: [FIREBASE LATENCY][ttl] - ");
+            Serial.print(firebaseData.errorReason());
+            Serial.println(".");
+        }
     }
 
     if(millis() >= time_now + 60000) {
@@ -205,9 +220,11 @@ void loop() {
                 }
             }
         } else {
-            Serial.print("[!] Error sending json cloud data: [FIREBASE LATENCY][ttl] - ");
-            Serial.print(firebaseData.errorReason());
-            Serial.println(".");
+            if(debug >= 1) {
+                Serial.print("[!] Error sending json cloud data: [FIREBASE LATENCY][ttl] - ");
+                Serial.print(firebaseData.errorReason());
+                Serial.println(".");
+            }
     
             digitalWrite(FAIL_PIN, HIGH);
 
